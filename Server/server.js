aa384544874a159b9cef24b11b145349ef16b0a2
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-06-08 10:33:53 +0530

    server

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://srivelen-client.vercel.app",
   }),
 );
 app.use(express.json());
