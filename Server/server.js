79612b4151fdda8ee1316a7436cb56d7eebdc103
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-06-08 10:26:09 +0530

    vercel

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 
 app.get("/api/health", (_req, res) => {
